f15c076b81880bab22a9c0a984c3c235af6c8254
leandrita <lea.bujh@gmail.com>
2023-11-16 14:00:44 +0100

    add routes in progress

--- server/routes/api.php
+++ server/routes/api.php
@@ -11,8 +11,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-
+Route::get('/interventions', [Controller::class, 'index']);
 
 Route::middleware(['auth:api'])->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/', [Controller::class, 'store']);
+    Route::get('//{id}', [Controller::class, 'show']);
+    Route::post('//{id}', [Controller::class, 'update']);
+    Route::delete('//{id}', [Controller::class, 'destroy']);
 });
