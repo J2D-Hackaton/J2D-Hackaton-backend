b6be8d37969cd38ded44017afe72e1295a2516ed
leandrita <lea.bujh@gmail.com>
2023-11-16 18:27:15 +0100

    update routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -8,20 +8,19 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/interventions', [InterventionController::class, 'index']);
 Route::get('/intervention/{id}', [InterventionController::class, 'show']);
 
-Route::middleware(['auth:api'])->group(function () {
+// Route::middleware(['auth:api'])->group(function () {
 
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/intervention', [InterventionController::class, 'store']);
-    Route::put('/intervention/{id}', [InterventionController::class, 'update']);
-    Route::delete('/intervention/{id}', [InterventionController::class, 'destroy']);
-
-});
+// Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/intervention', [InterventionController::class, 'store']);
+Route::put('/intervention/{id}', [InterventionController::class, 'update']);
+Route::delete('/intervention/{id}', [InterventionController::class, 'destroy']);
+// });
 
 Route::get('/all', [MultipolygonController::class, 'getAllMultipolygons'])->name('api.getAllMultipolygons');
 Route::get('/boroughs', [MultipolygonController::class, 'getCoordsFromAllBorough'])->name('api.getBoroughsCoords');
