a9a69b11f31414d4d7119c2e3735d356c9ab9310
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-16 15:13:07 +0100

    Partial MultiPolygonController did it

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\MultipolygonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::get('/all', [MultipolygonController::class, 'getAllMultipolygons'])->name('api.getAllMultipolygons');
+Route::get('/boroughs', [MultipolygonController::class, 'getBoroughsCoords'])->name('api.getBoroughsCoords');
